f2d0059f2b90a32738eda38f49e56d671a4603bf
Pranjal Satija <pranjal.satija@icloud.com>
2017-08-18 15:13:25 -0500

    Initial Commit

---
+++ Transform/Transform.h
@@ -0,0 +1,19 @@
+//
+//  Transform.h
+//  Transform
+//
+//  Created by Pranjal Satija on 8/18/17.
+//  Copyright © 2017 Pranjal Satija. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transform.
+FOUNDATION_EXPORT double TransformVersionNumber;
+
+//! Project version string for Transform.
+FOUNDATION_EXPORT const unsigned char TransformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transform/PublicHeader.h>
+
+
